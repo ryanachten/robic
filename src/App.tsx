04eff5bddd7d0a532f684fadb9f24a8f72c1ca49
Ryan Achten <ryanachten@gmail.com>
2018-12-15 14:28:50 +1300

    Init expo, typescript and jest config

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default () => (
+  <View style={{ marginTop: 50 }}>
+    <Text>Hello Expo!</Text>
+  </View>
+);
