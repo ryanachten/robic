5b752db0beed11da47cd313643874e16999755aa
Ryan Achten <ryanachten@gmail.com>
2020-08-23 15:22:41 +1200

    Decouple email and password state

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,26 +1,13 @@
-import React, { useState, Dispatch, SetStateAction } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input } from 'react-native-elements';
 
 import { Text, View } from '../components/Themed';
 
-type State = {
-  email: string;
-  password: string;
-};
-
 export default function LoginScreen() {
-  const [user, setUser]: [State, Dispatch<SetStateAction<State>>] = useState({
-    email: '',
-    password: '',
-  });
-
-  const updateUser = (property: 'email' | 'password', value: string) => {
-    const updatedUser = { ...user };
-    updatedUser[property] = value;
-    setUser(updatedUser);
-  };
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <View style={styles.container}>
@@ -29,16 +16,16 @@ export default function LoginScreen() {
         label="Email"
         placeholder="robic@user.com"
         leftIcon={<Icon name="envelope" size={24} color="black" />}
-        value={user.email}
-        onChange={(e) => updateUser('email', e.nativeEvent.text)}
+        value={email}
+        onChange={(e) => setEmail(e.nativeEvent.text)}
       />
       <Input
         label="Password"
         placeholder="••••••••••••"
         leftIcon={<Icon name="user" size={24} color="black" />}
-        value={user.password}
+        value={password}
         secureTextEntry={true}
-        onChange={(e) => updateUser('password', e.nativeEvent.text)}
+        onChange={(e) => setPassword(e.nativeEvent.text)}
       />
       <View
         style={styles.separator}
