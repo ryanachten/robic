a4bb2150a9fadd64ea35b474e669dfa9d26c8049
Ryan Achten <ryanachten@gmail.com>
2020-09-05 21:33:45 +1200

    Add register fields and signup handling to screen

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -5,15 +5,41 @@ import { Input, Card } from 'react-native-elements';
 import { AuthContext } from '../services/context';
 
 export default function RegisterScreen() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [signedUp, setSignedUp] = useState(false);
   const {
     state: { error },
     actions: { signUp },
   } = useContext(AuthContext);
+
+  const attemptSignUp = async () => {
+    await signUp({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+    setSignedUp(true);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Register</Text>
+      <Input
+        label="First name"
+        placeholder="John"
+        value={firstName}
+        onChange={(e) => setFirstName(e.nativeEvent.text)}
+      />
+      <Input
+        label="Last name"
+        placeholder="Smith"
+        value={lastName}
+        onChange={(e) => setLastName(e.nativeEvent.text)}
+      />
       <Input
         autoCapitalize="none"
         label="Email"
@@ -28,12 +54,18 @@ export default function RegisterScreen() {
         secureTextEntry={true}
         onChange={(e) => setPassword(e.nativeEvent.text)}
       />
+      {signedUp ? (
+        <Card containerStyle={styles.error} title="You're all signed up!">
+          <Text>Head to the login screen to get started</Text>
+          {/* TODO: should be modal with link to login screen */}
+        </Card>
+      ) : null}
       {error ? (
         <Card containerStyle={styles.error} title="Oops!">
           <Text>{error}</Text>
         </Card>
       ) : null}
-      <Button title="Register" onPress={() => signUp(email, password)} />
+      <Button title="Register" onPress={attemptSignUp} />
     </View>
   );
 }
