9fecc9f0297cb3d3cb38b735011fd2c5dbb6c062
Ryan Achten <ryanachten@gmail.com>
2020-09-29 18:05:26 +1300

    Add remove set button and method to ExerciseForm

--- components/ExerciseForm.tsx
+++ components/ExerciseForm.tsx
@@ -1,22 +1,20 @@
 import React, { useState, Dispatch, useEffect } from 'react';
 import { ExerciseDefinition, Set } from '../constants/Interfaces';
-import { ScrollView, View } from 'react-native';
-import { Input, Button } from 'react-native-elements';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Icon, Input, Button } from 'react-native-elements';
 
 export const ExerciseForm = ({
   definition: { id },
 }: {
   definition: ExerciseDefinition;
 }) => {
-  const initialSet: Set[] = [{ reps: 0, value: 5 }];
+  const initialSet: Set[] = [{ reps: 1, value: 5 }];
   const [sets, setSets]: [Set[], Dispatch<Set[]>] = useState(initialSet);
 
   // Reset form if definition ID changes
   useEffect(() => {
     setSets(initialSet);
   }, [id]);
-
-  // console.log('id', id);
 
   const updateSet = (index: number, field: 'reps' | 'value', value: string) => {
     const updatedSets = [...sets];
@@ -29,11 +27,18 @@ export const ExerciseForm = ({
     setSets(updatedSets);
   };
 
+  const removeSet = (index: number) => {
+    const updatedSets = [...sets];
+    updatedSets.splice(index, 1);
+    setSets(updatedSets);
+  };
+
   return (
-    <ScrollView>
+    <ScrollView contentContainerStyle={styles.container}>
       {sets.map(({ reps, value }: Set, index: number) => (
-        <View key={index}>
+        <View key={index} style={styles.setWrapper}>
           <Input
+            containerStyle={styles.input}
             label="Reps"
             keyboardType="numeric"
             value={reps ? reps.toString() : ''}
@@ -42,6 +47,7 @@ export const ExerciseForm = ({
             }
           />
           <Input
+            containerStyle={styles.input}
             label="Weight"
             keyboardType="numeric"
             value={value ? value.toString() : ''}
@@ -49,9 +55,28 @@ export const ExerciseForm = ({
               updateSet(index, 'value', nativeEvent.text)
             }
           />
+          {index > 0 && (
+            <Icon name="remove" raised onPress={() => removeSet(index)} />
+          )}
         </View>
       ))}
       <Button title="Add set" onPress={() => addSet()} />
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  setWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  input: {
+    width: '30%',
+  },
+});
