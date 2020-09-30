92261df009523dadbd4ba022413ad0913d66b962
Ryan Achten <ryan.achten@akqa.com>
2020-10-01 09:51:59 +1300

    Refactor Stopwatch using hooks

--- components/Stopwatch.tsx
+++ components/Stopwatch.tsx
@@ -1,126 +1,93 @@
 // Based on https://codersera.com/blog/first-react-native-app-stopwatch/ implementation
 
-import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React, { Dispatch, useState } from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 
 type Lap = { min: number; sec: number; msec: number };
 
-type Props = {};
-
-type State = {
-  start: boolean;
-  min: number;
-  sec: number;
-  msec: number;
-};
-
 let padToTwo = (number: number) => (number <= 9 ? `0${number}` : number);
 
-export class Stopwatch extends React.Component<Props, State> {
-  laps: Array<Lap>;
-  interval: NodeJS.Timeout | null;
+export const Stopwatch = () => {
+  // Stopwatch state
+  const [start, setStart] = useState(false);
+  const [msec, setMilliSec] = useState(0);
+  const [sec, setSec] = useState(0);
+  const [min, setMin] = useState(0);
+  const [interval, setIntervalTimestamp]: [
+    NodeJS.Timeout,
+    Dispatch<NodeJS.Timeout>
+  ] = useState({} as NodeJS.Timeout);
+  const [laps, setLaps]: [Array<Lap>, Dispatch<Array<Lap>>] = useState(
+    []
+  ) as any;
 
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      start: false,
-      min: 0,
-      sec: 0,
-      msec: 0,
-    };
-
-    this.laps = [];
-
-    this.interval = null;
-  }
-
-  handleToggle = () => {
-    this.setState(
-      {
-        start: !this.state.start,
-      },
-      () => this.handleStart()
-    );
+  const handleToggle = () => {
+    setStart(!start);
+    handleStart();
   };
 
-  handleLap = (min: number, sec: number, msec: number) => {
-    this.laps = [...this.laps, { min, sec, msec }];
+  const handleLap = (min: number, sec: number, msec: number) => {
+    setLaps([...laps, { min, sec, msec }]);
   };
 
-  handleStart = () => {
-    if (this.state.start) {
-      this.interval = setInterval(() => {
-        if (this.state.msec !== 99) {
-          this.setState({
-            msec: this.state.msec + 1,
-          });
-        } else if (this.state.sec !== 59) {
-          this.setState({
-            msec: 0,
-            sec: this.state.sec + 1,
-          });
+  const handleStart = () => {
+    if (start) {
+      const interval = setInterval(() => {
+        if (msec !== 99) {
+          setMilliSec(msec);
+        } else if (sec !== 59) {
+          setMilliSec(0);
+          setSec(sec + 1);
         } else {
-          this.setState({
-            msec: 0,
-            sec: 0,
-            min: this.state.min + 1,
-          });
+          setMilliSec(0);
+          setSec(0);
+          setMin(min + 1);
         }
       }, 1);
+      setIntervalTimestamp(interval);
     } else {
-      this.interval && clearInterval(this.interval);
+      interval && clearInterval(interval);
     }
   };
 
-  handleReset = () => {
-    this.setState({
-      min: 0,
-      sec: 0,
-      msec: 0,
-
-      start: false,
-    });
-
-    this.interval && clearInterval(this.interval);
-
-    this.laps = [];
+  const handleReset = () => {
+    setMilliSec(0);
+    setSec(0);
+    setMin(0);
+    setStart(false);
+    setLaps([]);
+    interval && clearInterval(interval);
   };
 
-  render() {
-    return (
-      <View>
-        <View style={styles.parent}>
-          <Text style={styles.child}>{padToTwo(this.state.min) + ' : '}</Text>
-          <Text style={styles.child}>{padToTwo(this.state.sec) + ' : '}</Text>
-          <Text style={styles.child}>{padToTwo(this.state.msec)}</Text>
-        </View>
-        <View style={styles.buttonParent}>
-          <TouchableOpacity onPress={this.handleReset}>
-            <Text style={styles.buttonText}>Reset</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.handleToggle}>
-            <Text style={styles.buttonText}>
-              {!this.state.start ? 'Start' : 'Stop'}
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() =>
-              this.handleLap(this.state.min, this.state.sec, this.state.msec)
-            }
-            disabled={!this.state.start}
-          >
-            <Text style={styles.buttonText}>Lap</Text>
-          </TouchableOpacity>
-        </View>
+  return (
+    <View>
+      <View style={styles.parent}>
+        <Text style={styles.child}>{padToTwo(min) + " : "}</Text>
+        <Text style={styles.child}>{padToTwo(sec) + " : "}</Text>
+        <Text style={styles.child}>{padToTwo(msec)}</Text>
       </View>
-    );
-  }
-}
+      <View style={styles.buttonParent}>
+        <TouchableOpacity onPress={handleReset}>
+          <Text style={styles.buttonText}>Reset</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleToggle}>
+          <Text style={styles.buttonText}>{!start ? "Start" : "Stop"}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => handleLap(min, sec, msec)}
+          disabled={!start}
+        >
+          <Text style={styles.buttonText}>Lap</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   parent: {
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
   },
 
   child: {
@@ -128,13 +95,13 @@ const styles = StyleSheet.create({
   },
 
   buttonParent: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
 
   buttonText: {
     fontSize: 20,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
 });
