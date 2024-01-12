29da291d1c048aa2c55f395906364e0903faa098
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2024-01-12 13:29:23 -0800

    fix: pass API URI via .env for dev

--- app/api/ApiClient.ts
+++ app/api/ApiClient.ts
@@ -1,14 +1,17 @@
 import createClient from "openapi-fetch";
 import { paths } from "./robic-openapi";
 
-// TODO: set via env variables
-const API_DOMAIN = "http://10.0.2.2:5000";
+/**
+ * EC2 Public IP
+ */
+const PUBLIC_API_URL = "http://35.174.97.134";
+const API_URL = process.env.EXPO_PUBLIC_API_URL ?? PUBLIC_API_URL;
 
 export const apiClientHeaders = {
   Authorization: "",
 };
 
 export const apiClient = createClient<paths>({
-  baseUrl: API_DOMAIN,
+  baseUrl: API_URL,
   headers: apiClientHeaders,
 });
