ddd5c3de2618396131eb1dbfa6f808aef6a4fbd4
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2024-01-13 09:29:39 -0800

    feat: add migrator user ID CLI arg

--- api/Robic.Migrator/Program.cs
+++ api/Robic.Migrator/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+namespace Robic.Migrator;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var userIdCommand = new Option<string>(name: "--userId", description: "User ID of the MongoDB user we want to migrate");
+        var rootCommand = new RootCommand("Migrate Robic resources from MongoDB to MySQL");
+        rootCommand.AddOption(userIdCommand);
+
+        rootCommand.SetHandler((userId) =>
+        {
+            Console.WriteLine($"UserId: {userId}");
+        }, userIdCommand);
+
+        await rootCommand.InvokeAsync(args);
+    }
+}
