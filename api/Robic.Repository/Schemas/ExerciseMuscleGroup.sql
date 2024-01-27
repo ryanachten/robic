1eed1dd398225c1ee07d9742583430ac505f45bb
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2024-01-27 14:41:20 -0800

    fix: add primary key to ExerciseMuscleGroup table

--- api/Robic.Repository/Schemas/ExerciseMuscleGroup.sql
+++ api/Robic.Repository/Schemas/ExerciseMuscleGroup.sql
@@ -1,4 +1,5 @@
 CREATE TABLE ExerciseMuscleGroup (
+  Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   MuscleCode VARCHAR(50) NOT NULL,
   DefinitionId INT NOT NULL,
   FOREIGN KEY (MuscleCode) REFERENCES MuscleGroup(MuscleCode),
