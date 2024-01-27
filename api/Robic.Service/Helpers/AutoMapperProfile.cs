7e4ed60890f68f52c628cab5064a16fedf6816d8
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2024-01-27 09:57:00 -0800

    fix: restrict float rounding to 2 dp

--- api/Robic.Service/Helpers/AutoMapperProfile.cs
+++ api/Robic.Service/Helpers/AutoMapperProfile.cs
@@ -4,6 +4,7 @@ using Robic.Service.Models.DTOs.Exercise;
 using Robic.Service.Models.DTOs.ExerciseDefinition;
 using Robic.Service.Models.DTOs.User;
 using Robic.Service.Models.Enums;
+using System;
 using RepositoryEnums = Robic.Repository.Models.Enums;
 using RepositoryModel = Robic.Repository.Models;
 
@@ -13,30 +14,43 @@ public class AutoMapperProfile : Profile
 {
     public AutoMapperProfile()
     {
+        // Repository -> Service mappings
         CreateMap<RepositoryModel.User, User>();
         CreateMap<RepositoryModel.User, UserDetailDto>();
         CreateMap<RepositoryModel.ExerciseDefinition, ExerciseDefinition>();
-        CreateMap<RepositoryModel.ExerciseDefinition, ExerciseDefinition>();
         CreateMap<RepositoryModel.ExerciseDefinitionSummary, ExerciseDefinitionSummary>();
         CreateMap<RepositoryModel.Exercise, Exercise>();
-        CreateMap<RepositoryModel.ExerciseSet, Set>();
-        CreateMap<RepositoryModel.PersonalBestMaxValues, PersonalBest>();
+        CreateMap<RepositoryModel.ExerciseSet, Set>()
+            .ForMember(
+                dest => dest.Value,
+                opt => opt.MapFrom(src => Math.Round(src.Value, 2))
+            );
+        CreateMap<RepositoryModel.PersonalBestMaxValues, PersonalBest>()
+            .ForMember(
+                dest => dest.TopAvgValue,
+                opt => opt.MapFrom<double?>(
+                    src => src.TopAvgValue.HasValue ? Math.Round(src.TopAvgValue.Value, 2) : null
+                ));
         CreateMap<RepositoryModel.ExerciseHistoryItem, ExerciseHistoryItem>();
         CreateMap<RepositoryModel.AnalyticsItem, AnalyticsItem>();
+
+        // Service -> Repository mappings
         CreateMap<Set, RepositoryModel.DTOs.Exercise.CreateExerciseSetDto>();
         CreateMap<RegisterUserDto, User>();
         CreateMap<User, UserDetailDto>();
         CreateMap<UpdateExerciseDefinitionDto, RepositoryModel.ExerciseDefinition>();
         CreateMap<UpdateExerciseDto, RepositoryModel.DTOs.Exercise.UpdateExerciseDto>();
+        CreateMap<UpdateExerciseDto, Exercise>()
+        .ForMember(
+            dest => dest.Sets,
+            opt => opt.MapFrom(src => src.Sets)
+        );
+
+        // Service enum -> Repository enum mappings
         CreateMap<SortDirection, RepositoryEnums.SortDirection>();
         CreateMap<ExerciseDefinitionSortField, RepositoryEnums.ExerciseDefinitionSortField>();
         CreateMap<MuscleGroup, RepositoryEnums.MuscleGroup>();
         CreateMap<Unit, RepositoryEnums.Unit>();
-        CreateMap<UpdateExerciseDto, Exercise>()
-            .ForMember(
-                dest => dest.Sets,
-                opt => opt.MapFrom(src => src.Sets)
-            );
     }
 
     public static IMapper CreateMapper()
