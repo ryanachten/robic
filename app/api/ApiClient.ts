633a815cc641154d64d594b00730ff2df225cbfe
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2024-01-27 18:47:36 -0800

    fix: enable HTTP traffic for production apps

--- app/api/ApiClient.ts
+++ app/api/ApiClient.ts
@@ -3,6 +3,7 @@ import { paths } from "./robic-openapi";
 
 /**
  * EC2 Public IP
+ * TODO: we really need HTTPS here...
  */
 const PUBLIC_API_URL = "http://35.174.97.134";
 const API_URL = process.env.EXPO_PUBLIC_API_URL ?? PUBLIC_API_URL;
